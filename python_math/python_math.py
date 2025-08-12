0068e04f929893e2d9f0bc3f451b9c77f6d4e935
sumansanyukta <48119132+sumansanyukta@users.noreply.github.com>
2025-08-12 09:35:36 +0200

    maths

---
+++ python_math/python_math.py
@@ -0,0 +1,30 @@
+# Built in maths function
+
+x = min(5,10,25)
+y = max(5,10,25)
+
+print(x)
+print(y)
+
+x = abs(-7.25)
+print(x)
+
+x = pow(4,3)
+print(x)
+
+# Math Module
+import math
+x=math.sqrt(64)
+print(x)
+
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)
+print(y)
+
+import math
+x = math.pi
+print(x)
