f6081a7a586f8c64110b3f19e563acd1251d5fae
sumansanyukta <48119132+sumansanyukta@users.noreply.github.com>
2025-08-13 09:49:47 +0200

    user input

---
+++ python_user_input/python_user_input.py
@@ -0,0 +1,11 @@
+#print("Enter your name:")
+#name = input()
+#print (f"Hello {name}")
+
+name = input("Enter your name:")
+print(f"Hello {name}")
+fav1 = input("What is your favorite animal:")
+fav2 = input("What is your favourite color:")
+fav3 = input("What is your favourite number:")
+
+print (f"DO you want a {fav1} {fav2} {fav3}")
